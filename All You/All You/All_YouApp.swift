27bebf13ee6589d10d1c702d1cf21634c8703e68
cadaniel <c.daniel.dev@gmail.com>
2023-05-16 08:48:31 -0400

    Add xcode project

---
+++ All You/All You/All_YouApp.swift
@@ -0,0 +1,17 @@
+//
+//  All_YouApp.swift
+//  All You
+//
+//  Created by Cate Daniel on 2023-05-16.
+//
+
+import SwiftUI
+
+@main
+struct All_YouApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
